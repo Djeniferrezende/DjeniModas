0ee327d9bddc04fbd46887fdb5994d7b2b09395d
Djeniferrezende <djenifer.rezende@gmail.com>
2021-04-26 19:03:51 -0300

    add logs no produto

--- src/main/java/com/djeniModas/Modas/Resource/ProdutoResource.java
+++ src/main/java/com/djeniModas/Modas/Resource/ProdutoResource.java
@@ -4,6 +4,8 @@ import com.djeniModas.Modas.model.Categoria;
 import com.djeniModas.Modas.model.Produto;
 import com.djeniModas.Modas.service.CategoriaService;
 import com.djeniModas.Modas.service.ProdutoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +20,23 @@ import java.util.List;
 @RequestMapping(value = "/produtos")
 public class ProdutoResource {
 
+    //implementação de logs
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProdutoResource.class);
+
     @Autowired
     ProdutoService service;
 
     @GetMapping
     public ResponseEntity<List<Produto>> findAll(){
     List<Produto> produto = service.findAll();
+    LOGGER.info("listagem produto finalizada");
     return ResponseEntity.ok().body(produto);
 }
     @GetMapping("/{id}")
     public ResponseEntity<Produto> findById(@PathVariable Long id){
         Produto produto = service.finById(id);
+        LOGGER.info("listagem produto por id finalizada");
+
         return ResponseEntity.ok().body(produto);
     }
 
--- src/main/java/com/djeniModas/Modas/service/ProdutoService.java
+++ src/main/java/com/djeniModas/Modas/service/ProdutoService.java
@@ -4,6 +4,8 @@ import com.djeniModas.Modas.model.Categoria;
 import com.djeniModas.Modas.model.Produto;
 import com.djeniModas.Modas.repository.CategoriaRepository;
 import com.djeniModas.Modas.repository.ProdutoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,22 @@ import java.util.Optional;
 @Service
 public class ProdutoService {
 
+    //implementação de logs
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProdutoService.class);
+
+
     @Autowired
     ProdutoRepository repository;
 
 
     public List<Produto> findAll(){
+        LOGGER.info("listando produtos");
         return repository.findAll();
     }
 
     public Produto finById(Long id){
         Optional<Produto> cat = repository.findById(id);
+        LOGGER.info("bucando produtos por id");
        return cat.get();
 
     }
